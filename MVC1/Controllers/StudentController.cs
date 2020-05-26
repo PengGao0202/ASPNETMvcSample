ad49ff84f17af57ee27c554dc1191581988fd27a
oldGunCoder <57119330+oldGunCoder@users.noreply.github.com>
2020-05-26 14:49:56 -0400

    connect to database StudentDB

--- MVC1/Controllers/StudentController.cs
+++ MVC1/Controllers/StudentController.cs
@@ -1,10 +1,11 @@
-﻿using MVC1.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC1.DataAccessLayer;
+using System.Data;
+using System.Net;
 
 namespace MVC1.Controllers
 {
@@ -15,108 +16,158 @@ namespace MVC1.Controllers
         // GET: Student
         public ActionResult Index()
         {
-            //MvcApplication.studentsList.Clear();
-            //MvcApplication.studentsList.Add(new Models.Student { StudentId = 1, StudentName = "Peng1", Age = 19 });
-            //MvcApplication.studentsList.Add(new Models.Student { StudentId = 2, StudentName = "Peng2", Age = 29 });
-            //string queryString = "SELECT OrderID, CustomerID FROM dbo.Orders;";
-            //using (SqlConnection connection = new SqlConnection(
-            //           connectionString))
-            //{
-            //    SqlCommand command = new SqlCommand(
-            //        queryString, connection);
-            //    connection.Open();
-            //    using (SqlDataReader reader = command.ExecuteReader())
-            //    {
-            //        while (reader.Read())
-            //        {
-            //            Console.WriteLine(String.Format("{0}, {1}",
-            //                reader[0], reader[1]));
-            //        }
-            //    }
-            //}
-
-
-            return View(MvcApplication.studentsList);
+            //return View(MvcApplication.studentsList);
+            return View(DBContext.SelectAllStudents());
         }
 
         // GET: Student/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            Student student = new Student();
-            student = MvcApplication.studentsList.Find(x => x.StudentId.Equals(id));
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            Models.Student student = DBContext.SelectStudentById(id);
+
+            if(student == null)
+            {
+                return HttpNotFound();
+            }
+            
             return View(student);
         }
 
         // GET: Student/Create
         public ActionResult Create()
         {
+                        
             return View();
         }
 
         // POST: Student/Create
         [HttpPost]
-        public ActionResult Create(Student student)
+        public ActionResult Create(Models.Student student)
         {
             try
             {
                 // TODO: Add insert logic here
-                student.StudentId = ++ MvcApplication.globalStudentId;
-                MvcApplication.studentsList.Add(student);
-                return RedirectToAction("Index");
+                //student.StudentId = ++ MvcApplication.globalStudentId;
+                //MvcApplication.studentsList.Add(student);
+                if(ModelState.IsValid)
+                {
+                    if (student == null) { return HttpNotFound(); }          
+                        
+                    DBContext.CreateStudent(student);
+                    
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again");
+                
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                Models.Student student = DBContext.SelectStudentById(id);
+                if(student == null) { return HttpNotFound(); }
+
+                return View(student);
             }
             catch
             {
                 return View();
             }
-        }
-
-        // GET: Student/Edit/5
-        public ActionResult Edit(int id)
-        {
-            
-            Student student = new Student();
-            student = MvcApplication.studentsList.Find(x => x.StudentId.Equals(id));
-            return View(student);
-
         }
 
         // POST: Student/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, Student student)
+        public ActionResult Edit(int? id, Models.Student student)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (student == null) { return HttpNotFound(); 
+            }
             try
             {
                 // TODO: Add update logic here
-                int index = MvcApplication.studentsList.FindIndex(x => x.StudentId.Equals(id));
-                MvcApplication.studentsList[index] = student;
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    DBContext.UpdateStudentById(id, student);
+                    return RedirectToAction("Index");
+                }
+                   
+                
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again");
+                
+            }
+
+            return View(student);
+
+
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                Models.Student student = DBContext.SelectStudentById(id);
+                if(student == null) { return HttpNotFound(); }
+
+                return View(student);
             }
             catch
             {
                 return View();
             }
-        }
-
-        // GET: Student/Delete/5
-        public ActionResult Delete(int id)
-        {
-            Student student = new Student();
-            student = MvcApplication.studentsList.Find(x => x.StudentId.Equals(id));
-            return View(student);
+        
         }
 
         // POST: Student/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, Student student)
+        public ActionResult Delete(int? id, Models.Student student)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (student == null) { return HttpNotFound(); }
+                        
             try
             {
-                // TODO: Add delete logic here
-                int index = MvcApplication.studentsList.FindIndex(x => x.StudentId.Equals(id));
-                MvcApplication.studentsList.RemoveAt(index);
+                // TODO: Add update logic here
+                DBContext.DeleteStudentById(id);
                 return RedirectToAction("Index");
             }
-            catch
+            catch 
             {
                 return View();
             }
